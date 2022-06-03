4440c723738abd4d23b8492c1485ed290fe0654a
jihyelee <jihyelee.329@gmail.com>
2022-06-03 22:05:28 +0900

    solution/6kyu : build tower 주석추가

--- codewar/6kyu/Build Tower.js
+++ codewar/6kyu/Build Tower.js
@@ -1,12 +1,12 @@
 function towerBuilder(nFloors) {
-      // build here
-      const floors =[]; //빈배열 
-      
-      for(let n =1; n < nFloors +1; n++){
-        const stars = '*'.repeat(n*2 -1);
-        const space = ' '.repeat(nFloors - n);
-        const result = `${space}${stars}${space}`
-        floors.push(result);
-      }
-      return floors
+  const floors =[]; //빈배열 
+  
+  // 숫자1부터 반복문 시작, nFloors+1  수보다 작을 때 까지 반복
+  for(let n =1; n < nFloors +1; n++){
+    const stars = '*'.repeat(n*2 -1);  // '*' 문자를 n 숫자가 증가함에 따라, 2개씩 추가되어 * 반복출력
+    const space = ' '.repeat(nFloors - n);  //마지막 * 층에는 공백이 없어야하므로, nFloors - 현재 숫자(n)
+    const result = `${space}${stars}${space}` // 최종 출력
+    floors.push(result);
+  }
+  return floors
 }
