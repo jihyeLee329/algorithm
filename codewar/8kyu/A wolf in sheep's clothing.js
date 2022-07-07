4b56952bd2d3ddf6fe94be62bdd60ca2552da91a
Emma <jihyelee.329@gmail.com>
2022-07-07 17:40:33 +0900

    solution/8kyu : A wolf in sheep's clothing

--- codewar/8kyu/A wolf in sheep's clothing.js
+++ codewar/8kyu/A wolf in sheep's clothing.js
@@ -1,6 +1,6 @@
 function warnTheSheep(queue) {
-  const sheep_num = queue.length - queue.indexOf("wolf") - 1;
-  return sheep_num > 0 ? `Oi! Sheep number ${sheep_num}! You are about to be eaten by a wolf!` : "Pls go away and stop eating my sheep";
+  const sheep_num = queue.reverse().indexOf("wolf");
+  return sheep_num === 0 ? `Oi! Sheep number ${sheep_num}! You are about to be eaten by a wolf!` : "Pls go away and stop eating my sheep";
 }
 
 warnTheSheep(["sheep", "sheep", "wolf"]);
