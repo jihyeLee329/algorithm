282a123cddad62d085bb64aa993c3e97b2e45028
Emma <jihyelee.329@gmail.com>
2022-07-07 11:27:51 +0900

    solution/8kyu : Compare within margin

--- codewar/8kyu/Compare within margin.js
+++ codewar/8kyu/Compare within margin.js
@@ -1,12 +1,4 @@
 function closeCompare(a, b, margin) {
-  let MARGIN = margin;
-  if (margin === undefined) {
-    MARGIN = 0;
-  }
   const distance = Math.abs(a - b);
-  if (MARGIN >= distance) {
-    return 0;
-  } else {
-    return a - b > 0 ? 1 : -1;
-  }
+  margin >= distance ? 0 : a - b > 0 ? 1 : -1;
 }
