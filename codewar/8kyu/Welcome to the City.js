f2244110299fda90643586e1c95afc387bd5a02b
Emma <jihyelee.329@gmail.com>
2022-07-22 16:12:30 +0900

    solution/8kyu : Welcome to the City

--- codewar/8kyu/Welcome to the City.js
+++ codewar/8kyu/Welcome to the City.js
@@ -1,5 +1,3 @@
-function sayHello( name, city, state ) {
-
-    return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`;
-    
+function sayHello(name, city, state) {
+  return `Hello, ${name.join(" ")}! Welcome to ${city}, ${state}!`;
 }
